848bcde4e5f75698d5bb5fab070abb03335de867
Satipov Akbar <88106359+codewithakbar@users.noreply.github.com>
2024-04-26 10:44:10 +0500

    sex 3

--- config/settings.py
+++ config/settings.py
@@ -128,8 +128,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+import os 
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = 'media/'

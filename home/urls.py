90a5f7e53bef0a2b9ca8ae38acc7ca9db7f693aa
Satipov Akbar <88106359+codewithakbar@users.noreply.github.com>
2024-04-26 12:29:48 +0500

    shu yaxshi

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from .views import MaktablarQRCodeAPIView
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'maktablar', MaktablarQRCodeAPIView)
 
 urlpatterns = [
-    path('maktablar/<slug:slug>/', MaktablarQRCodeAPIView.as_view(), name='maktablar_qr_code'),
+    path('', include(router.urls)),
 ]
--- home/views.py
+++ home/views.py
@@ -8,7 +8,7 @@ from PIL import Image
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import permissions, viewsets
 
 from django.conf import settings
 from django.shortcuts import get_object_or_404, render
@@ -21,45 +21,11 @@ from .models import Maktablar
 
 
 
-class MaktablarQRCodeAPIView(APIView):
-    def get(self, request, slug):
-        maktab = get_object_or_404(Maktablar, slug=slug)
-        
-        qr = qrcode.QRCode(
-            version=1,
-            error_correction=qrcode.constants.ERROR_CORRECT_L,
-            box_size=10,
-            border=4,
-        )
-        
-        # qr.add_data("https://youtube.com/")
-        qr.add_data(request.build_absolute_uri())
-        qr.make(fit=True)
+class MaktablarQRCodeAPIView(viewsets.ModelViewSet):
+    queryset = Maktablar.objects.all()
+    serializer_class = MaktablarSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-        img = qr.make_image(fill_color="black", back_color="white")
-
-        buffer = BytesIO()
-        img.save(buffer, format='PNG')
-        
-        buffer.seek(0)
-        
-        # Fayl nomini generatsiya qilish
-        filename = f"qr-{maktab.slug}.png"
-        file_path = os.path.join(settings.MEDIA_ROOT, filename)
-        
-        # Agar avvalgi versiya mavjud bo'lsa, o'chiramiz
-        if os.path.exists(file_path):
-            os.remove(file_path)
-        
-        # Faylni saqlash
-        fs = FileSystemStorage(location=settings.MEDIA_ROOT)
-        filepath = fs.save(filename, buffer)
-        image_url = fs.url(filepath)
-        
-        serializer = MaktablarSerializer(maktab)
-        # Javobda rasmlar uchun URL-ni qo'shamiz
-        return Response({'maktab': serializer.data, 'qr_image_url': image_url}, status=status.HTTP_200_OK)
-    
 
 
 def home_page(request):

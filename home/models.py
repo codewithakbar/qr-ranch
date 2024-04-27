86f615017a40900eb431c74ae8a8cbafcc272f2c
Satipov Akbar <88106359+codewithakbar@users.noreply.github.com>
2024-04-27 11:00:29 +0500

    daylight

--- home/models.py
+++ home/models.py
@@ -7,6 +7,7 @@ class Maktablar(models.Model):
     name = models.CharField(max_length=233, verbose_name="Maktab nomi")
     direktor_name = models.CharField(max_length=233, verbose_name="Direktor nomi")
     slug = models.SlugField(unique=True, max_length=255, verbose_name="Slug")
+    full_link = models.URLField(blank=True, null=True)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
--- home/serializers.py
+++ home/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import Maktablar
 
-class MaktablarSerializer(serializers.ModelSerializer):
+class MaktablarSerializer(serializers.ModelSerializer):\
+
+    full_link = serializers.SerializerMethodField()
+
     class Meta:
         model = Maktablar
         fields = '__all__'
+
+
+    def get_full_link(self, obj):
+        return f"https://school.utu-ranch.uz/{obj.slug}"
